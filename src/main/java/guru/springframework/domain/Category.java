05dd65ca24b3c72cf6a7626adf41e4cc76fd01ab
maxproof <max.karberg@gmail.com>
2018-01-02 19:40:28 +0100

    Logging and exclusion for hashcode

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -1,11 +1,13 @@
 package guru.springframework.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipes"}) //exclu circularité (loop => stack overflow) des relations bidirectionnelles dans le hashcode
 @Entity
 public class Category {
 
